0cdb3d5804ec1fc8f6f2d46bd2882f9931a4fabb
royhe911 <42162741+royhe911@users.noreply.github.com>
2018-08-15 16:16:40 +0800

    添加发短信方法

--- application/admin/common.php
+++ application/admin/common.php
@@ -17,7 +17,7 @@ if (!function_exists('buildSelect')) {
      */
     function buildSelect($name, $options, $selected = [], $attr = [])
     {
-        $options = is_array($options) ? $options : explode(',', $options);
+        $options  = is_array($options) ? $options : explode(',', $options);
         $selected = is_array($selected) ? $selected : explode(',', $selected);
         return Form::select($name, $options, $selected, $attr);
     }
@@ -34,7 +34,7 @@ if (!function_exists('buildRadios')) {
      */
     function buildRadios($name, $list = [], $selected = null)
     {
-        $html = [];
+        $html     = [];
         $selected = is_null($selected) ? key($list) : $selected;
         $selected = is_array($selected) ? $selected : explode(',', $selected);
         foreach ($list as $k => $v) {
@@ -55,7 +55,7 @@ if (!function_exists('buildCheckboxs')) {
      */
     function buildCheckboxs($name, $list = [], $selected = null)
     {
-        $html = [];
+        $html     = [];
         $selected = is_null($selected) ? [] : $selected;
         $selected = is_array($selected) ? $selected : explode(',', $selected);
         foreach ($list as $k => $v) {
@@ -97,35 +97,35 @@ if (!function_exists('buildToolbar')) {
      * @param array $attr 按钮属性值
      * @return string
      */
-    function buildToolbar($btns = NULL, $attr = [])
+    function buildToolbar($btns = null, $attr = [])
     {
-        $auth = \app\admin\library\Auth::instance();
+        $auth       = \app\admin\library\Auth::instance();
         $controller = str_replace('.', '/', strtolower(think\facade\Request::instance()->controller()));
-        $btns = $btns ? $btns : ['refresh', 'add', 'edit', 'del', 'import', 'status_enable', 'status_disable'];
-        $btns = is_array($btns) ? $btns : explode(',', $btns);
-        $index = array_search('delete', $btns);
-        if ($index !== FALSE) {
+        $btns       = $btns ? $btns : ['refresh', 'add', 'edit', 'del', 'import', 'status_enable', 'status_disable'];
+        $btns       = is_array($btns) ? $btns : explode(',', $btns);
+        $index      = array_search('delete', $btns);
+        if ($index !== false) {
             $btns[$index] = 'del';
         }
         $btnAttr = [
-            'refresh' => ['javascript:;', 'btn btn-primary btn-refresh', 'fa fa-refresh', '', __('Refresh')],
-            'add'     => ['javascript:;', 'btn btn-success btn-add', 'fa fa-plus', __('Add'), __('Add')],
-            'edit'    => ['javascript:;', 'btn btn-success btn-edit btn-disabled disabled', 'fa fa-pencil', __('Edit'), __('Edit')],
-            'del'     => ['javascript:;', 'btn btn-danger btn-del btn-disabled disabled', 'fa fa-trash', __('Delete'), __('Delete')],
-            'import'  => ['javascript:;', 'btn btn-danger btn-import', 'fa fa-upload', __('Import'), __('Import')],
+            'refresh'        => ['javascript:;', 'btn btn-primary btn-refresh', 'fa fa-refresh', '', __('Refresh')],
+            'add'            => ['javascript:;', 'btn btn-success btn-add', 'fa fa-plus', __('Add'), __('Add')],
+            'edit'           => ['javascript:;', 'btn btn-success btn-edit btn-disabled disabled', 'fa fa-pencil', __('Edit'), __('Edit')],
+            'del'            => ['javascript:;', 'btn btn-danger btn-del btn-disabled disabled', 'fa fa-trash', __('Delete'), __('Delete')],
+            'import'         => ['javascript:;', 'btn btn-danger btn-import', 'fa fa-upload', __('Import'), __('Import')],
             'status_enable'  => ['javascript:;', 'btn btn-success btn-status-enable btn-disabled disabled', 'fa fa-trash', __('StatusEnable'), __('StatusEnable')],
-            'status_disable'  => ['javascript:;', 'btn btn-success btn-status-disable btn-disabled disabled', 'fa fa-trash', __('StatusDisable'), __('StatusDisable')],
+            'status_disable' => ['javascript:;', 'btn btn-success btn-status-disable btn-disabled disabled', 'fa fa-trash', __('StatusDisable'), __('StatusDisable')],
         ];
         $btnAttr = array_merge($btnAttr, $attr);
-        $html = [];
+        $html    = [];
         foreach ($btns as $k => $v) {
             //如果未定义或没有权限
             if (!isset($btnAttr[$v]) || ($v !== 'refresh' && !$auth->check("{$controller}/{$v}"))) {
                 continue;
             }
             list($href, $class, $icon, $text, $title) = $btnAttr[$v];
-            $extend = $v == 'import' ? 'id="btn-import-file" data-url="ajax/upload" data-mimetype="csv,xls,xlsx" data-multiple="false"' : '';
-            $html[] = '<a href="' . $href . '" class="' . $class . '" title="' . $title . '" ' . $extend . '><i class="' . $icon . '"></i> ' . $text . '</a>';
+            $extend                                   = $v == 'import' ? 'id="btn-import-file" data-url="ajax/upload" data-mimetype="csv,xls,xlsx" data-multiple="false"' : '';
+            $html[]                                   = '<a href="' . $href . '" class="' . $class . '" title="' . $title . '" ' . $extend . '><i class="' . $icon . '"></i> ' . $text . '</a>';
         }
         return implode(' ', $html);
     }
@@ -139,22 +139,22 @@ if (!function_exists('buildHeading')) {
      * @param string $path 指定的path
      * @return string
      */
-    function buildHeading($path = NULL, $container = TRUE)
+    function buildHeading($path = null, $container = true)
     {
         $title = $content = '';
         if (is_null($path)) {
-            $action = request()->action();
+            $action     = request()->action();
             $controller = str_replace('.', '/', request()->controller());
-            $path = strtolower($controller . ($action && $action != 'index' ? '/' . $action : ''));
+            $path       = strtolower($controller . ($action && $action != 'index' ? '/' . $action : ''));
         }
         // 根据当前的URI自动匹配父节点的标题和备注
         $data = Db::name('auth_rule')->where('name', $path)->field('title,remark')->find();
         if ($data) {
-            $title = __($data['title']);
+            $title   = __($data['title']);
             $content = __($data['remark']);
         }
 //        if (!$content)
-//            return '';
+        //            return '';
         $result = '<div class="panel-lead"><em>' . $title . '</em>' . $content . '</div>';
         if ($container) {
             $result = '<div class="panel-heading">' . $result . '</div>';
@@ -163,8 +163,7 @@ if (!function_exists('buildHeading')) {
     }
 }
 
-if (!function_exists('rmdirs'))
-{
+if (!function_exists('rmdirs')) {
 
     /**
      * 删除文件夹
@@ -174,8 +173,10 @@ if (!function_exists('rmdirs'))
      */
     function rmdirs($dirname, $withself = true)
     {
-        if (!is_dir($dirname))
+        if (!is_dir($dirname)) {
             return false;
+        }
+
         $files = new RecursiveIteratorIterator(
             new RecursiveDirectoryIterator($dirname, RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST
         );
@@ -191,8 +192,7 @@ if (!function_exists('rmdirs'))
     }
 }
 
-if (!function_exists('cdnurl'))
-{
+if (!function_exists('cdnurl')) {
 
     /**
      * 获取上传资源的CDN的地址
@@ -202,5 +202,40 @@ if (!function_exists('cdnurl'))
     function cdnurl($url)
     {
         return preg_match("/^https?:\/\/(.*)/i", $url) ? $url : \think\facade\Config::get('upload.cdnurl') . $url;
+    }
+}
+
+if (!function_exists('sendsms')) {
+    /**
+     * 发送短信
+     * @param  string  $to    手机号码集合，多个用英文逗号分开
+     * @param  array   $data  内容数据 格式为数组 例如：array('Marry','Alon')，如不需替换请填 null
+     * @param  integer $temId 模板Id,测试应用和未上线应用使用测试模板请填写1，正式应用上线后填写已申请审核通过的模板ID
+     * @param  string  $appId 应用Id，在官网应用列表中点击应用，对应应用详情中的APP ID
+     * @return bool           返回发送结果
+     */
+    function sendsms($to = '', $data = [], $temId = 1, $appId = '8a216da85e6fff2b015e79280b69054c')
+    {
+        // 主帐号,对应开官网发者主账号下的 ACCOUNT SID
+        $accountSid = '8a216da85e6fff2b015e79280b1a0547';
+        // 主帐号令牌,对应官网开发者主账号下的 AUTH TOKEN
+        $accountToken = '993ee1fa02e7418d8d48f07a79b0eab3';
+        //沙盒环境（用于应用开发调试）：sandboxapp.cloopen.com
+        //生产环境（用户应用上线使用）：app.cloopen.com
+        $serverIP = 'app.cloopen.com';
+        //请求端口，生产环境和沙盒环境一致
+        $serverPort = '8883';
+
+        //REST版本号，在官网文档REST介绍中获得。
+        $softVersion = '2013-12-26';
+        $rest        = new \Org\Net\REST($serverIP, $serverPort, $softVersion);
+        $rest->setAccount($accountSid, $accountToken);
+        $rest->setAppId($appId);
+        // 发送模板短信
+        $result = $rest->sendTemplateSMS($to, $datas, $tempId);
+        if (empty($result) || $result->statusCode != 0) {
+            return false;
+        }
+        return true;
     }
 }
