314db269ea0ac730554eef26c0af8f8fbb4990de
royhe911 <42162741+royhe911@users.noreply.github.com>
2018-08-15 17:19:04 +0800

    代码格式化

--- application/admin/controller/merchant/Merchant.php
+++ application/admin/controller/merchant/Merchant.php
@@ -1,13 +1,12 @@
 <?php
 namespace app\admin\controller\merchant;
 
+use app\admin\model\AdminLog;
+use app\common\controller\Backend;
+use app\common\model\Merchant as MerchantM;
 use think\facade\Config;
 use think\facade\Hook;
 use think\Validate;
-
-use app\admin\model\AdminLog;
-use app\common\model\Merchant as MerchantM;
-use app\common\controller\Backend;
 
 /**
  * 后台首页
@@ -16,17 +15,21 @@ use app\common\controller\Backend;
 class Merchant extends Backend
 {
     protected $relationSearch = true;
-    protected $layout = 'default2';
+    protected $layout         = 'default2';
 
     /**
      * 后台首页
      */
     public function index()
     {
-        $w = [];
+        $w       = [];
         $keyword = request()->get('keyword');
-        if ($keyword != null) $w[] = ['m.username|m.mobile|m.wx_id', 'like', '%' . $keyword . '%'];
-        if (in_array(12, $this->auth->getGroupIds())) { // 业务员
+        if ($keyword != null) {
+            $w[] = ['m.username|m.mobile|m.wx_id', 'like', '%' . $keyword . '%'];
+        }
+
+        if (in_array(12, $this->auth->getGroupIds())) {
+            // 业务员
             $w[] = ['a.id', '=', session('admin')['id']];
         }
         $list = (new MerchantM)
@@ -45,45 +48,38 @@ class Merchant extends Backend
     public function login()
     {
         $url = $this->request->get('url', 'index/index');
-        if ($this->auth->isLogin())
-        {
+        if ($this->auth->isLogin()) {
             return $this->redirect($url);
         }
-        if ($this->request->isPost())
-        {
-            $username = $this->request->post('username');
-            $password = $this->request->post('password');
+        if ($this->request->isPost()) {
+            $username  = $this->request->post('username');
+            $password  = $this->request->post('password');
             $keeplogin = $this->request->post('keeplogin');
             //$token = $this->request->post('__token__');
             $rule = [
-                'username'  => 'require|length:3,30',
-                'password'  => 'require|length:3,30',
+                'username' => 'require|length:3,30',
+                'password' => 'require|length:3,30',
                 //'__token__' => 'token',
             ];
             $data = [
-                'username'  => $username,
-                'password'  => $password,
+                'username' => $username,
+                'password' => $password,
                 //'__token__' => $token,
             ];
-            if (Config::get('fastadmin.login_captcha'))
-            {
+            if (Config::get('fastadmin.login_captcha')) {
                 $rule['captcha'] = 'require|captcha';
                 $data['captcha'] = $this->request->post('captcha');
             }
             $validate = new Validate($rule, [], ['username' => __('Username'), 'password' => __('Password'), 'captcha' => __('Captcha')]);
-            $result = $validate->check($data);
-            if (!$result)
-            {
+            $result   = $validate->check($data);
+            if (!$result) {
                 return $this->result(null, 0, $validate->getError(), 'json');
             }
             AdminLog::setTitle(__('Login'));
             $result = $this->auth->login($username, $password, $keeplogin ? 86400 : 0);
-            if ($result === true)
-            {
+            if ($result === true) {
                 return $this->result(['url' => $url], 1, '登录成功', 'json');
-            }
-            else
-            {
+            } else {
                 $msg = $this->auth->getError();
                 $msg = $msg ? $msg : '账号或密码输入有误';
                 return $this->result(null, 0, $msg, 'json');
@@ -91,8 +87,7 @@ class Merchant extends Backend
         }
 
         // 根据客户端的cookie,判断是否可以自动登录
-        if ($this->auth->autologin())
-        {
+        if ($this->auth->autologin()) {
             $this->redirect($url);
         }
         $background = cdnurl(Config::get('fastadmin.login_background'));
